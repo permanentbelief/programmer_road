7d107b1dbb5ff6e9ea0cf4dd7dafbce2422ac4b8
permenent <2691556679@qq.com>
2019-11-27 22:35:23 +0800

    commit

---
+++ day20_公共字串的计算.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	string str1;
+	string str2;
+	cin >> str1 >> str2;
+	int  m = str1.size();
+	int n = str2.size();
+	vector<vector<int>> v(m, vector<int>(n, 0));
+	for (int i = 0; i < m; i++)
+	{
+		if (str1[i] == str2[0])
+		{
+			v[i][0] = 1;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (str2[i] == str1[0])
+		{
+			v[0][i] = 1;
+		}
+	}
+	for (int i = 1; i < m; i++)
+	{
+		for (int j = 1; j < n; j++)
+		{
+			if (str1[i] == str2[j])
+			{
+				v[i][j] = v[i - 1][j - 1] + 1;
+			}
+		}
+	}
+	int max = v[0][0];
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (max  < v[i][j])
+			{
+				max = v[i][j];
+			}
+		}
+	}
+	cout << max << endl;
+	system("pause");
+}
