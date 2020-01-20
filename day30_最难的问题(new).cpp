5bcfc135f51413743af6a86b6ca20200ae93a516
permenent <2691556679@qq.com>
2020-01-20 11:38:07 +0800

    commit

---
+++ day30_最难的问题(new).cpp
@@ -0,0 +1,37 @@
+//#define _CRT_SECURE_NO_WARNINGS 1 
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+////int  main()
+////{
+////	string str;
+////	static string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+////	while (getline(cin, str))
+////	{
+////		string outcome;
+////		for (int i = 0; i < str.size(); i++)
+////		{
+////			if (str[i] == ' ')
+////				outcome += ' ';
+////			else
+////				outcome += s[abs((str[i] - 'A' + 21)) % 26];
+////		}
+////		cout << outcome << endl;
+////	}
+////	system("pause");
+////}
+//
+//int main()
+//{
+//	char c;
+//	while ((c = getchar()) != EOF)
+//	{
+//		if ('A' <= c && c <= 'Z')
+//		{
+//			if ( c = (c > 'E') ? (c - 5) : (c + 21));
+//		}
+//		putchar(c);
+//	}
+//	system("pause");
+//}
