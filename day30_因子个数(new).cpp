f97b9bb05f76d26dd79b4563a66407cd6b07cd81
permenent <2691556679@qq.com>
2020-01-20 11:37:24 +0800

    commit

---
+++ day30_因子个数(new).cpp
@@ -0,0 +1,32 @@
+//#define _CRT_SECURE_NO_WARNINGS 1 
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <cmath>
+//using namespace std;
+//
+//int main()
+//{
+//	int Integer = 0;
+//	while (cin >> Integer)
+//	{
+//		int count = 0;
+//		for (int i = 2; i < sqrt(Integer); i++)
+//		{
+//			if (Integer % i == 0)
+//			{
+//				while (Integer % i == 0)
+//				{
+//					Integer /= i;
+//				}
+//				count++;
+//			}
+//
+//		}
+//		if (Integer != 1)
+//			count++;
+//		cout << count << endl;
+//	}
+//	system("pause");
+//}
+//
