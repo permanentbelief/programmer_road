f321a4e75e9a6193b59ad70881ec1f8429e4e49f
permenent <2691556679@qq.com>
2020-01-22 17:36:13 +0800

    commit

---
+++ 面试指南/替换字符串中连续出现的指定字符串.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+string Replace(string str, string from, string to)
+{
+	for (int i = 0; i < str.size(); i++)
+	{
+		s
+	}
+}
+int main()
+{
+}
+	
+	
