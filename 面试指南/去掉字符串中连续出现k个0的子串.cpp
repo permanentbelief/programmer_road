db3ef5cdde8c41e06314d723f2ba5144805b3d2d
permenent <2691556679@qq.com>
2020-01-23 18:29:16 +0800

    commit

--- 面试指南/去掉字符串中连续出现k个0的子串.cpp
+++ 面试指南/去掉字符串中连续出现k个0的子串.cpp
@@ -1,49 +1,49 @@
-//#define _CRT_SECURE_NO_WARNINGS 1 
-//
-//#include <iostream>
-//#include <algorithm>
-//#include <string>
-//#include <vector>
-//#include <stack>
-//#include <queue>
-//
-//using namespace std;
-//
-//string removeKzeros(string str, int k)
-//{
-//	int count = 0;
-//	int flag = 0;
-//	for (int i = 0; i < str.size(); i++) // a00e
-//	{
-//		count = 0;
-//		while (i < str.size() && str[i] == '0')
-//		{
-//			count++;
-//			i++;
-//			if (count == k)
-//			{
-//				break;
-//			}
-//		}
-//		if (count == k)
-//		{
-//			flag = i;
-//			break;
-//		}
-//	}
-//	for (int i = flag - count; i + k < str.size(); i++)
-//	{
-//		str[i] = str[i + k];
-//	}
-//	for (int i = 0; i < k; i++)
-//	{
-//		str.erase(--str.end());
-//	}
-//	return str;
-//}
-//int main()
-//{
-//	string str = "A00B000CCC";
-//	cout << removeKzeros(str, 3) << endl;
-//	system("pause");
-//}
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+string removeKzeros(string str, int k)
+{
+	int count = 0;
+	int flag = 0;
+	for (int i = 0; i < str.size(); i++) // a00e
+	{
+		count = 0;
+		while (i < str.size() && str[i] == '0')
+		{
+			count++;
+			i++;
+			if (count == k)
+			{
+				break;
+			}
+		}
+		if (count == k)
+		{
+			flag = i;
+			break;
+		}
+	}
+	for (int i = flag - count; i + k < str.size(); i++)
+	{
+		str[i] = str[i + k];
+	}
+	for (int i = 0; i < k; i++)
+	{
+		str.erase(--str.end());
+	}
+	return str;
+}
+int main()
+{
+	string str = "A00B000CCC";
+	cout << removeKzeros(str, 3) << endl;
+	system("pause");
+}
