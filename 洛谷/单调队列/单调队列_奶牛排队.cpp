6ecb3c2d7db5bfd8f5fb99ccb433639f79a3488b
permenent <2691556679@qq.com>
2020-11-07 21:51:32 +0800

    commit

---
+++ 洛谷/单调队列/单调队列_奶牛排队.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+
+int main()
+{
+	
+	system("pause");
+}
